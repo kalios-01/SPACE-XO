abf9e2181e27fd0bccf96c0c2142487accd3fcea
kalios-01 <priyanshu07893@gmail.com>
2024-10-09 20:48:34 +0530

    added background music

---
+++ spacexo/app/src/main/java/com/kaliostech/spacexo/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.kaliostech.spacexo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // Start the new activity here
+            Intent intent = new Intent(SplashScreen.this, PlayerChoice.class);
+            startActivity(intent);
+            // Close the SplashScreen activity
+            finish();
+        }, 2000); // 3000 milliseconds delay
+
+    }
+}
